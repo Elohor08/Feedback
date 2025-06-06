87e3a28cf7533f132a83cfee6cff852ad595eb61
Aghanokpe <atelohor@gmail.com>
2025-06-06 17:54:31 +0100

    finished work

--- server/server.js
+++ server/server.js
@@ -25,4 +25,5 @@ const feedbackRoutes = require('./routes/feedback');
 app.use('/api/feedback', feedbackRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
+
